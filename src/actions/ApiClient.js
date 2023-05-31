c50467a2643326a21132fa2a07a4b2e7b49114de
HienBull98 <phunghien9812@gmail.com>
2023-05-31 11:04:03 +0700

    init project

---
+++ src/actions/ApiClient.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import Swal from "sweetalert2";
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_URL_API,
+});
+let errorCatch = false;
+instance.interceptors.response.use(
+    function (config) {
+        errorCatch = false;
+        return config;
+    },
+    function (error) {
+        if (error.response && error.response.status) {
+            if (error.response.status === 401) {
+                if (!errorCatch) {
+                    //Swal.warning("Tài khoản hết hiệu lực sử dụng");
+                    Swal.fire({
+                        text: "Tài khoản hết hiệu lực sử dụng",
+                        icon: "warning",
+                    }).then(() => {
+                        localStorage.clear();
+                        window.location.reload(false);
+                    });
+                }
+                errorCatch = true;
+            }
+        }
+        return Promise.reject(error);
+    },
+);
